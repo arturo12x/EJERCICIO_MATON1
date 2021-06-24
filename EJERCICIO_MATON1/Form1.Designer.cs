ea13d92246000509cb2dae800524f63eb203c175
Jazhiel Arturo Cossío López <jazhiel12a@gmail.com>
2021-06-24 00:26:34 -0500

    Initial commit

---
+++ EJERCICIO_MATON1/Form1.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace EJERCICIO_MATON1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.test = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.BTNRESET = new System.Windows.Forms.Button();
+            this.BTNGET = new System.Windows.Forms.Button();
+            this.CBLEGALP = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.TXTLAST = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TXTNAME = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(36)))), ((int)(((byte)(73)))));
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.test);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(1, -6);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(816, 497);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::EJERCICIO_MATON1.Properties.Resources.ShadySociableAzurewingedmagpie_max_1mb;
+            this.pictureBox3.Location = new System.Drawing.Point(408, 249);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(265, 149);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 3;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            this.pictureBox3.WaitOnLoad = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(335, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(253, 38);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "YOUR RFC IS:";
+            this.label5.Visible = false;
+            // 
+            // test
+            // 
+            this.test.AutoSize = true;
+            this.test.Font = new System.Drawing.Font("Century", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.test.ForeColor = System.Drawing.Color.White;
+            this.test.Location = new System.Drawing.Point(335, 130);
+            this.test.Name = "test";
+            this.test.Size = new System.Drawing.Size(0, 38);
+            this.test.TabIndex = 1;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(179)))), ((int)(((byte)(142)))), ((int)(((byte)(93)))));
+            this.panel2.Controls.Add(this.BTNRESET);
+            this.panel2.Controls.Add(this.BTNGET);
+            this.panel2.Controls.Add(this.CBLEGALP);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.dateTimePicker1);
+            this.panel2.Controls.Add(this.TXTLAST);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.TXTNAME);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(0, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(329, 457);
+            this.panel2.TabIndex = 0;
+            // 
+            // BTNRESET
+            // 
+            this.BTNRESET.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(36)))), ((int)(((byte)(73)))));
+            this.BTNRESET.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BTNRESET.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.BTNRESET.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTNRESET.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNRESET.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.BTNRESET.Location = new System.Drawing.Point(218, 398);
+            this.BTNRESET.Name = "BTNRESET";
+            this.BTNRESET.Size = new System.Drawing.Size(98, 43);
+            this.BTNRESET.TabIndex = 9;
+            this.BTNRESET.Text = "RESET";
+            this.BTNRESET.UseVisualStyleBackColor = false;
+            this.BTNRESET.Click += new System.EventHandler(this.BTNRESET_Click);
+            // 
+            // BTNGET
+            // 
+            this.BTNGET.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(157)))), ((int)(((byte)(36)))), ((int)(((byte)(73)))));
+            this.BTNGET.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.BTNGET.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.BTNGET.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BTNGET.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BTNGET.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.BTNGET.Location = new System.Drawing.Point(82, 320);
+            this.BTNGET.Name = "BTNGET";
+            this.BTNGET.Size = new System.Drawing.Size(169, 52);
+            this.BTNGET.TabIndex = 8;
+            this.BTNGET.Text = "GET";
+            this.BTNGET.UseVisualStyleBackColor = false;
+            this.BTNGET.Click += new System.EventHandler(this.BTNGET_Click);
+            // 
+            // CBLEGALP
+            // 
+            this.CBLEGALP.AutoSize = true;
+            this.CBLEGALP.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.CBLEGALP.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.CBLEGALP.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.CBLEGALP.Font = new System.Drawing.Font("Century", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CBLEGALP.Location = new System.Drawing.Point(110, 277);
+            this.CBLEGALP.Name = "CBLEGALP";
+            this.CBLEGALP.Size = new System.Drawing.Size(159, 22);
+            this.CBLEGALP.TabIndex = 7;
+            this.CBLEGALP.Text = "LEGAL PERSON";
+            this.CBLEGALP.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(0, 223);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 40);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "DATE OF\r\nBIRTH";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(110, 223);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(216, 20);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // TXTLAST
+            // 
+            this.TXTLAST.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TXTLAST.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.TXTLAST.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXTLAST.Location = new System.Drawing.Point(110, 186);
+            this.TXTLAST.Name = "TXTLAST";
+            this.TXTLAST.Size = new System.Drawing.Size(216, 20);
+            this.TXTLAST.TabIndex = 4;
+            this.TXTLAST.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TXTLAST_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(0, 186);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "LASTNAME";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Controls.Add(this.pictureBox2);
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Location = new System.Drawing.Point(0, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(329, 117);
+            this.panel3.TabIndex = 2;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(1)))));
+            this.pictureBox2.Image = global::EJERCICIO_MATON1.Properties.Resources._600px_SHCP_Logo_2019_svg;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(200, 43);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(1)))));
+            this.pictureBox1.Image = global::EJERCICIO_MATON1.Properties.Resources.sat;
+            this.pictureBox1.Location = new System.Drawing.Point(274, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(55, 43);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(51, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(218, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "GET YOUR RFC\r\n";
+            // 
+            // TXTNAME
+            // 
+            this.TXTNAME.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.TXTNAME.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.TXTNAME.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TXTNAME.Location = new System.Drawing.Point(110, 145);
+            this.TXTNAME.Name = "TXTNAME";
+            this.TXTNAME.Size = new System.Drawing.Size(216, 20);
+            this.TXTNAME.TabIndex = 1;
+            this.TXTNAME.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.TXTNAME_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "NAME (S)";
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel4.Controls.Add(this.label2);
+            this.panel4.Location = new System.Drawing.Point(0, 49);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(329, 65);
+            this.panel4.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "|1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox TXTNAME;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox CBLEGALP;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox TXTLAST;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button BTNRESET;
+        private System.Windows.Forms.Button BTNGET;
+        private System.Windows.Forms.Label test;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ EJERCICIO_MATON1/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EJERCICIO_MATON1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BTNGET_Click(object sender, EventArgs e)
+        {
+            //INICIALIZACION
+            string lastname = "";
+            string name = "";
+            string birth = "";
+            string RFC = "";
+            string letter = "";
+            int contespacio = 0;
+            bool validar = false;
+            String ERROR = "";
+            TXTNAME.BackColor = Color.White;
+            TXTLAST.BackColor = Color.White;
+            //ASIGNACION
+            lastname = TXTLAST.Text;
+            name = TXTNAME.Text;
+            birth = dateTimePicker1.Value.ToShortDateString(); //23/05/2018
+
+            if (string.IsNullOrWhiteSpace(TXTNAME.Text)) {
+                ERROR += " NO PUEDES DEJAR VACIO EL CAMPO NAME";
+                TXTNAME.BackColor = Color.DarkRed;
+             
+            }
+
+
+
+            if (string.IsNullOrWhiteSpace(TXTLAST.Text))
+            {
+                ERROR += " NO PUEDES DEJAR VACIO EL CAMPO LAST NAME";
+                TXTLAST.BackColor = Color.DarkRed;
+             
+            }
+
+            if (ERROR != "")
+            {
+                MessageBox.Show(ERROR, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+ 
+
+            //NOMBRES
+            RFC += name.Substring(0, 1);
+            contespacio = 0;
+            for (int i = 0; i < name.Length; i++)
+            {
+                letter = "";
+                letter = name.Substring(i, 1);
+
+                //BUSCAR ESPACIOS EN BLANCO jaz jaz jaz jaz jaz
+                if (letter == " ")
+                {
+                    contespacio = contespacio+1;
+                    //VERIFICAR QUE NO EXISTAN MAS DE 4 NOMBRES (4 ESPACIOS O MAS)
+                    if (contespacio > 3)
+                    {
+                        MessageBox.Show("SOLO PUEDES ESCRIBIR HASTA UN MAXIMO DE 4 NOMBRES","ERROR", MessageBoxButtons.OK,MessageBoxIcon.Error);
+                        TXTNAME.BackColor = Color.DarkRed;
+                        return;
+
+
+
+                    }
+                    RFC += name.Substring(i + 1, 1);
+                }
+            }
+            //APELLIDOS
+            contespacio = 0;
+                for(int i=0; i < lastname.Length; i++)
+                {
+                    letter = "";
+
+                letter = lastname.Substring(i, 1);
+                if (letter == " ") {
+                    //VERIFICAR QUE SOLO EXISTAN 2 APELLIDOS
+                    contespacio = contespacio + 1;
+                    if (contespacio > 1 )
+                    {
+                        MessageBox.Show("SOLO PUEDES ESCRIBIR UN MAXIMOD DE 2 APELLIDOS", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                       TXTLAST.BackColor = Color.DarkRed;
+                        return;
+                    }
+
+                    //PRIMER APELLIDO
+                    RFC += lastname.Substring(i-2 , 2);
+                //SEGUNDO APELLIDO
+
+                RFC += lastname.Substring(lastname.Length-2,2);
+                   
+                }
+               
+      
+            }
+                //VALIDACION SI SOLO ESCRIBIO UN APELLIDO
+            if (contespacio == 0)
+            {
+                MessageBox.Show("SE NECESITAN 2 APELLIDOS", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                TXTLAST.BackColor = Color.DarkRed;
+                return;
+            }
+                
+         
+
+            //FORMATO FECHA DIA MES AÑO  20/12/1999
+            //FECHA
+
+            //AÑO RFC
+            RFC += birth.Substring(birth.Length - 2, 2);
+
+            for (int i = birth.Length-1;i>=0; i--)
+            {
+                letter = "";
+
+                letter = birth.Substring(i,1);
+                if (letter == "/")
+                {
+                    RFC += birth.Substring(i - 2, 2);
+                }
+            }
+
+            //SI ES PERSONA FISICA (CHECKBOX=TRUE)
+            if (CBLEGALP.Checked == true)
+            {
+
+                //ULTIMA LETRA DEL PRIMER NOMBRE
+                for (int i = 0; i < name.Length; i++)
+                {
+                    letter = "";
+                    letter = name.Substring(i, 1);
+                    if (letter == " ")
+                    {
+                        RFC += name.Substring(i - 1, 1);
+                        validar = true;
+                        break;
+
+                    }
+
+             
+                }
+
+                if (validar == false)
+                {
+                    RFC += name.Substring(name.Length - 1, 1);
+                }
+                //ULTIMA LETRA DEL PRIMER APELLIDO
+                for (int i = 0; i < lastname.Length; i++)
+                {
+                    letter = "";
+
+                    letter = lastname.Substring(i, 1);
+                    if (letter == " ")
+                    {
+
+                        RFC += lastname.Substring(i - 1, 1);
+                        break;
+                    }
+
+                }
+                //  PRIMER DIGITO DEL MES
+                for (int i = 0; i < birth.Length; i++)
+                {
+                    letter = "";
+
+                    letter = birth.Substring(i, 1);
+                    if (letter == "/")
+                    {
+                        RFC += birth.Substring(i + 1, 1);
+                        break;
+                    }
+                }
+
+
+
+            }
+            label5.Visible = true;
+            pictureBox3.Visible = true;
+                test.Text =RFC;
+        }
+
+        private void BTNRESET_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+
+        private void TXTNAME_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //VALIDACION PARA QUE SOLO ACEPTE LETRAS Y TECLAS DE CONTROL
+            e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Back|| e.KeyChar == (char)Keys.Space);
+        }
+
+        private void TXTLAST_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //VALIDACION PARA QUE SOLO ACEPTE LETRAS Y TECLAS DE CONTROL
+            e.Handled = !(char.IsLetter(e.KeyChar) || e.KeyChar == (char)Keys.Back || e.KeyChar == (char)Keys.Space);
+        }
+    }
+}
